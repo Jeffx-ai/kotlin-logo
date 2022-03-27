db795e7a024fe4fbc6879ad118c95edf74ef15c3
Geoff <85445401+Jeffx-ai@users.noreply.github.com>
2022-03-27 09:34:18 +0300

    Create Logo.kt

---
+++ Logo.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val si=8
+    fun fL(x:Int): Int = x*2
+    val mS= Array(11,{ i -> i + 3})
+    for(i in 0..(fL(si))){
+        for(j in i until mS[8])
+         print("KK")
+        for(j in mS[2] until i)
+         if(!(i>=mS[8]))
+          print("TTTT")
+        println()
+    }
+}
